e0b7586e4f3de496cb38503bed0fc7af1388be81
treva556 <marktreva92@gmail.com>
2023-12-30 16:38:35 +0300

    new

---
+++ src/pages/Activities.js
@@ -0,0 +1,16 @@
+
+// activities
+import React from 'react';
+import './App.css';
+
+function Activities() {
+  return (
+    <div className="App">
+  <h1 className="text-3xl font-bold underline">
+      Sankara Hotels!
+    </h1>
+      </div>
+  );
+}
+
+export default Activities;
---
+++ src/pages/header.js
@@ -0,0 +1,17 @@
+
+
+// header
+import React from 'react';
+import './App.css';
+
+function Header() {
+  return (
+    <div className="App">
+  <h1 className="text-3xl font-bold underline">
+      Sankara Hotels!
+    </h1>
+      </div>
+  );
+}
+
+export default Header;
